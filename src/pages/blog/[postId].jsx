01ed2dad24600a57fda76aa797fbe86de2c26bb3
Juan Naranjo <juan.naranjo@dreamcodesoft.com>
2025-04-23 09:31:51 -0500

    fix delete script

--- src/pages/blog/[postId].jsx
+++ src/pages/blog/[postId].jsx
@@ -20,10 +20,7 @@ export default function Post({ post }) {
 
   // Si el post no existe, mostramos la página 404
   if (!post) {
-    const metaRobots = document.createElement('meta');
-    metaRobots.name = 'robots';
-    metaRobots.content = 'noindex';
-    document.head.appendChild(metaRobots);
+    
     return <NotFound />;
   }
 
